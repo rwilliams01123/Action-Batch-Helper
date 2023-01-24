0622a93759ae359dbffdc119a04a35868a9cfd6d
rwilliams01123 <rwilliams01123@gmail.com>
2023-01-23 19:37:27 -0500

    setup updated

--- setup.py
+++ setup.py
@@ -3,5 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='batch_helper',
     version='0.1.0',
-    packages=find_packages()
+    packages=find_packages(include=[
+        'batch_helper',
+        'batch_helper.*'])
 )
